7e793fe0624879a7c5d9a6beb45a72c65841b987
Revaldyhfz <revaldyhafiz16@gmail.com>
2024-05-08 20:11:37 +0700

    More decorating

--- src/components/chat.rs
+++ src/components/chat.rs
@@ -145,7 +145,7 @@ impl Component for Chat {
                     {
                         self.users.clone().iter().map(|u| {
                             html!{
-                                <div class="flex m-3 bg-white rounded-lg p-2">
+                                <div class="flex m-3 bg-blue rounded-lg p-2">
                                     <div>
                                         <img class="w-12 h-12 rounded-full" src={u.avatar.clone()} alt="avatar"/>
                                     </div>
@@ -154,7 +154,7 @@ impl Component for Chat {
                                             <div>{u.name.clone()}</div>
                                         </div>
                                         <div class="text-xs text-blue-400">
-                                            {"Hi there!"}
+                                            {"ONLINE"}
                                         </div>
                                     </div>
                                 </div>
@@ -190,7 +190,7 @@ impl Component for Chat {
 
                     </div>
                     <div class="w-full h-14 flex px-3 items-center">
-                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-blue-100 rounded-full outline-none focus:text-gray-700" name="message" required=true />
+                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-blue-100 rounded-full outline-none focus:text-blue-700" name="message" required=true />
                         <button onclick={submit} class="p-3 shadow-sm bg-blue-600 w-10 h-10 rounded-full flex justify-center items-center color-white">
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
                                 <path d="M0 0h24v24H0z" fill="none"></path><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
